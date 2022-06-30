d6f8c992f0ea9191aad62d28e2dc01f9b0797133
patrickp2502 <patrick.pradka@gmail.com>
2022-06-30 14:00:58 +0200

    added fetchCartValue

--- src/main/java/com/codecool/shop/checkout/controller/CheckoutController.java
+++ src/main/java/com/codecool/shop/checkout/controller/CheckoutController.java
@@ -1,6 +1,17 @@
 package com.codecool.shop.checkout.controller;
 
+import com.codecool.shop.config.RouteConfiguration;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.google.gson.JsonObject;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -10,6 +21,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.net.URISyntaxException;
 
 @WebServlet(urlPatterns = {"/checkout"}, loadOnStartup = 5)
 public class CheckoutController extends HttpServlet {
@@ -18,6 +30,9 @@ public class CheckoutController extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine templateEngine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+        String sessionId = req.getRequestedSessionId();
+        double cartValue = fetchCartValue(sessionId);
+        context.setVariable("value", cartValue);
 
         templateEngine.process("checkout/index.html", context, resp.getWriter());
 
@@ -25,11 +40,20 @@ public class CheckoutController extends HttpServlet {
     }
 
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("worked post");
-        resp.sendRedirect("/checkout");
 
+    private double fetchCartValue(String sessionId) {
+        System.out.println("Receiving");
+        HttpClient client = HttpClientBuilder.create().build();
+        URIBuilder builder = new URIBuilder(RouteConfiguration.URI_API_GET_CART_VALUE);
+        builder.setParameter("id", sessionId);
 
+        try {
+            HttpGet request = new HttpGet(builder.build());
+            HttpResponse response = client.execute(request);
+
+        } catch (IOException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+        return 9999;
     }
 }
--- src/main/java/com/codecool/shop/config/RouteConfiguration.java
+++ src/main/java/com/codecool/shop/config/RouteConfiguration.java
@@ -6,11 +6,12 @@ import java.net.URISyntaxException;
 public class RouteConfiguration {
 
     public final static URI URI_CART_TRANSFER;
+    public final static URI URI_API_GET_CART_VALUE;
 
     static {
         try {
             URI_CART_TRANSFER = new URI("http://0.0.0.0:8080/api/cart");
-
+            URI_API_GET_CART_VALUE = new URI("http://0.0.0.0:8080/api/cart/session");
 
 
         } catch (URISyntaxException e) {
